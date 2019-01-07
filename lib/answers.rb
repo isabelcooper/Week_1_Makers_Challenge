7a5867f93ec05cb130dedf9ba417f8cd027e2d9e
Paul <paul.ayling@bglgroup.co.uk>
2019-01-07 14:22:41 +0000

    added class

--- lib/answers.rb
+++ lib/answers.rb
@@ -1,3 +1,13 @@
-def add_one(array)
-  array.map { |x| x += 1 }
+class Answers
+  def sort(array)
+    array.sort
+  end
+
+  def sort_and_increment(array)
+    add_one(array).sort
+  end
+
+  def add_one(array)
+    array.map { |x| x += 1 }
+  end
 end
--- spec/answers_spec.rb
+++ spec/answers_spec.rb
@@ -1,8 +1,18 @@
 require 'answers'
 
-describe 'add_one' do
-  it "Returns array with each element incremented by 1" do
-  expect(add_one([1,2,3,4,5])).to eq [2,3,4,5,6]
-end
+describe Answers do
+#  let(:answer_ordered) {Answers.new([1,2,3,4,5])}
+  describe 'add_one' do
+    it "Returns array with each element incremented by 1" do
+      expect(Answers.new.add_one([1,2,3,4,5])).to eq [2,3,4,5,6]
+    end
 
+    it "sorts array smallest to largest" do
+      expect(Answers.new.sort([1,3,5,4,2])).to eq [1,2,3,4,5]
+    end
+
+    it "sorts and increments array" do
+      expect(Answers.new.sort_and_increment([1,3,5,4,2])).to eq [2,3,4,5,6]
+    end
+  end
 end
