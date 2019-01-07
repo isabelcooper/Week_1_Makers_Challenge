b232c92bcd6775413fbda65842e4163ac424b422
Paul <paul.ayling@bglgroup.co.uk>
2019-01-07 14:48:29 +0000

    first hash comit

--- lib/answers.rb
+++ lib/answers.rb
@@ -10,4 +10,18 @@ class Answers
   def add_one(array)
     array.map { |x| x += 1 }
   end
+
+  def sum(array)
+    array.inject(0, :+)
+  end
+
+  def twice_sum(array)
+    sum(array) * 2
+  end
+
+  def increment_hash(hash)
+    hash.each { |x, y| hash[x] += 1 }
+    hash
+  end
+
 end
--- spec/answers_spec.rb
+++ spec/answers_spec.rb
@@ -14,5 +14,18 @@ describe Answers do
     it "sorts and increments array" do
       expect(Answers.new.sort_and_increment([1,3,5,4,2])).to eq [2,3,4,5,6]
     end
+
+    it "sums the array" do
+      expect(Answers.new.sum([1,2,3,4,5])).to eq 15
+    end
+
+    it "sums the array and doubles it" do
+      expect(Answers.new.twice_sum([1,2,3,4,5])).to eq 30
+    end
+
+    it "increments each value in a hash" do
+      expect(Answers.new.increment_hash({ a: 1, b: 2 })).to eq ({ a: 2, b: 3 })
+    end
+
   end
 end
